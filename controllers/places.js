f6cdf70ce3500dac1383a2a8d37120e1a89744fe
imandato <imandato211@gmail.com>
2022-08-21 11:29:39 -0400

    Adding bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -8,13 +8,13 @@ router.get('/', (req,res) =>{
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/res1.jpg'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/res2.jpg'
       }]
     res.render('places/index', {places})
 })
