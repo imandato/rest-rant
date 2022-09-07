67dfa943bba902a401eebcd27c4b54cf811f8fd8
imandato <imandato211@gmail.com>
2022-09-06 22:48:43 -0400

    added show page

--- controllers/places.js
+++ controllers/places.js
@@ -26,10 +26,18 @@ router.post('/', (req, res) => {
   })
 })
 
-
-router.get('/new', (req, res) => {
-  res.render('places/new')
+// show
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
+
 
 router.get('/:id', (req, res) => {
   res.send('GET /places/:id stub')
