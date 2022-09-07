d3bcf36e57be267850a704db19cbaf66fc2d5716
imandato <imandato211@gmail.com>
2022-09-06 23:00:35 -0400

    seed data working

--- models/places.js
+++ models/places.js
@@ -12,17 +12,3 @@ const placeSchema = new mongoose.Schema({
 module.exports = mongoose.model('Place', placeSchema)
 
 
-
-// module.exports = [{
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: '/images/res1.jpg'
-// }, {
-//     name: 'Coding Cat Cafe',
-//     city: 'Phoenix',
-//     state: 'AZ',
-//     cuisines: 'Coffee, Bakery',
-//     pic: '/images/res2.jpg'
-// }]
